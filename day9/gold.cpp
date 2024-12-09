91e8c0207cb3a45179209df424e923c3b2084d2a
leske42 <mria.marika.huszr@gmail.com>
2024-12-09 17:59:09 +0100

    reorganizing works better but need to account for split space

--- day9/gold.cpp
+++ day9/gold.cpp
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 10:12:49 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/12/09 11:13:53 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/12/09 17:55:50 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include<fstream>
 #include<iostream>
 
-typedef std::vector<char> Line;
+typedef std::vector<std::pair<char, int>> Line;
 typedef std::vector<int> Dotmap;
 Dotmap dotmap;
 #define DOT 2147483647
@@ -30,50 +30,20 @@ void add_up_dotmap()
     }
 }
 
-bool contains_empty_space(int& idx)
-{
-    Dotmap::iterator iter = dotmap.begin();
-    while (iter + idx != dotmap.end())
-    {
-        if (*(iter + idx) == DOT)
-            return true;
-        idx++;
-    }
-    return false;
-}
-
-void move_memory()
-{
-    int idx = 0;
-    while (contains_empty_space(idx))
-    {
-        int back = DOT;
-        while (back == DOT)
-        {
-            back = dotmap.back();
-            dotmap.pop_back();
-        }
-        dotmap[idx] = back;
-        idx++;
-    }
-}
-
 void create_dotmap(Line line)
 {
-    int file_idx = -1;
     int idx = 0;
     while (idx < line.size())
     {
-        if (line[idx] == -1)
+        if (line[idx].first == -1)
         {
             idx++;
             continue;
         }
-        file_idx++;
-        int amount = line[idx] - 48;
+        int amount = line[idx].first - 48;
         while (amount)
         {
-            dotmap.push_back(file_idx);
+            dotmap.push_back(line[idx].second);
             amount--;
         }
         idx++;
@@ -84,19 +54,19 @@ void reorganize_line(Line& line)
 {
     int idx = 1;
     int reverse_idx = line.size() - 1;
-    while (idx < reverse_idx)
+    while (idx <= reverse_idx)
     {
-        if (line[idx] > line[reverse_idx])
+        if (line[idx].first >= line[reverse_idx].first)
         {
             line[idx] = line[reverse_idx];
-            line[reverse_idx] = -1;
+            line[reverse_idx].first = -1;
             idx += 2;
         }
         reverse_idx -= 2;
     }
     while (idx < line.size()) //remove all SPC so now we consider line has files only
     {
-        line[idx] = -1;
+        line[idx].first = -1;
         idx += 2;
     }
 }
@@ -115,11 +85,20 @@ int main(void)
     std::ifstream file("proba.txt");
     std::string content;
     file >> content;
-    std::cout << content.size() << std::endl; //DEBUGINFO: check if line ends with a file or emtpy space
-    Line line(content.begin(), content.end());
+    std::cout << "Line len: " << content.size() << std::endl; //DEBUGINFO: check if line ends with a file or emtpy space
+    int idx = 0;
+    int file_idx = 0;
+    Line line;
+    while (idx < content.size())
+    {
+        std::pair<char, int> newpair(content[idx], file_idx);
+        line.push_back(newpair);
+        if (idx % 2 == 0)
+            file_idx++;
+        idx++;
+    }
     reorganize_line(line);
     create_dotmap(line);
-    //move_memory();
     add_up_dotmap();
     print_dotmap();
     std::cout << res << std::endl;
